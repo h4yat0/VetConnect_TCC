89e81c8ce6aee06f0a8290f35ff149bc817c6ba6
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-04-24 23:14:14 -0300

    tailwind colors and fonts update

--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [
-    "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: { inter: ["Inter"] },
+			colors: {
+				vetConnectPrimaryGreen: "#4ecca3",
+				vetConnectSecundaryGreen: "#8be5bb",
+				vetConnectGray: "#393e46",
+				vetConnectWhite: "#eeeeee",
+			},
+		},
 	},
 	plugins: [],
 };
