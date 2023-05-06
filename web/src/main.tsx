b4617dbc25527c295238293cbc60b42a5d003986
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-05-06 16:31:43 -0300

    [UPDATE]  componentes movidos para app

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,17 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { App } from "./App";
 
 import './index.css'
-import Navbar from './components/shared/Navbar'
-import Footer from './components/shared/Footer'
-import ContentHome from './components/content/ContentHome'
 
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Navbar/>
-    <ContentHome/>
-    <Footer/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
