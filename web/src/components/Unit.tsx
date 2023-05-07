394c474cc9832bf147cb17559ed228d41a149cbb
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-05-07 14:58:06 -0300

    [ADD] componente de unidade

---
+++ web/src/components/Unit.tsx
@@ -0,0 +1,18 @@
+interface UnitProps {
+	title: string;
+	imgPath: string;
+}
+export default function Unit(props: UnitProps) {
+	const { title, imgPath } = props;
+
+	return (
+		<div className='font-inter'>
+			<img
+				src={imgPath}
+				alt=''
+				className='h-36 w-[365px] shadow-md rounded-lg object-cover'
+			/>
+			<h3 className='pt-1 font-semibold text-lg'> {title}</h3>
+		</div>
+	);
+}
