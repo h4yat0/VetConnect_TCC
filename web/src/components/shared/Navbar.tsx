671a794180f3dd2b31e794e64961aab4690b3247
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-05-06 22:18:55 -0300

    [UPDATE] navbar

--- web/src/components/shared/Navbar.tsx
+++ web/src/components/shared/Navbar.tsx
@@ -1,49 +1,69 @@
 import "../../styles/components/navbar.css";
-import "../../styles/btnHamburger.css";
-import svgLogo from '../../assets/svgs/vetConnectLogo.svg';
+import ButtonPrimary from "../buttons/ButtonSecundary";
 
-function Navbar() {
-  return (
-   <header>
-      <div className="navbar">
-      <img src={svgLogo} className="svgLogo" alt="SVG image" />
-        <div className="logo">
-          <a href="#">
-         
-           VetConnect
-          </a>
-        </div>
-        <ul className="itens">
-          <li><a href="#">Home</a></li>
-          <li><a href="#">Sobre</a></li>
-          <li><a href="#">Serviços</a></li>
-          <li><a href="#">Contato</a></li>
-          <li><a href="#">Agendamento</a></li>
-        </ul>
-        <a href="#" className="navbar_btn">Seja Cliente</a>
-        <div className="toogle_btn">
-        <input type="checkbox" id="menu_checkbox"/>
-      <label htmlFor="menu_checkbox">
-        <div></div>
-        <div></div>
-        <div></div>
-      </label>
-      <div className="dropdown_menu">
-          <ul className="">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Sobre</a></li>
-              <li><a href="#">Serviços</a></li>
-              <li><a href="#">Contato</a></li>
-              <li><a href="#">Agendamento</a></li>
-              <li><a href="#" className="">Seja Cliente</a></li>
-            </ul>
-        </div>
-        </div> 
-         
-      
-      </div>
-   </header>
-  );
+import vetConnectLogo from "../../assets/svgs/vetConnectLogo.svg";
+
+export default function Navbar() {
+	return (
+		<header>
+			<div className='navbar'>
+				<img src={vetConnectLogo} className='svgLogo' alt='SVG image' />
+				<div className='logo'>
+					<a href='#'>VetConnect</a>
+				</div>
+				<ul className='itens'>
+					<li>
+						<a href='#'>Home</a>
+					</li>
+					<li>
+						<a href='#'>Sobre</a>
+					</li>
+					<li>
+						<a href='#'>Serviços</a>
+					</li>
+					<li>
+						<a href='#'>Contato</a>
+					</li>
+					<li>
+						<a href='#'>Agendamento</a>
+					</li>
+				</ul>
+				<a href='signup'>
+					<ButtonPrimary text='Seja Cliente' />
+				</a>
+				<div className='toogle_btn'>
+					<input type='checkbox' id='menu_checkbox' />
+					<label htmlFor='menu_checkbox'>
+						<div></div>
+						<div></div>
+						<div></div>
+					</label>
+					<div className='dropdown_menu'>
+						<ul className=''>
+							<li>
+								<a href='#'>Home</a>
+							</li>
+							<li>
+								<a href='#'>Sobre</a>
+							</li>
+							<li>
+								<a href='#'>Serviços</a>
+							</li>
+							<li>
+								<a href='#'>Contato</a>
+							</li>
+							<li>
+								<a href='#'>Agendamento</a>
+							</li>
+							<li>
+								<a href='#' className=''>
+									Seja Cliente
+								</a>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</header>
+	);
 }
-
-export default Navbar
