d138798cc1217ccbfe94daa23465d45efb8ca090
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-06-03 02:38:10 -0300

    [FIX] services

--- server/src/main/java/com/example/api/service/AnimalService.java
+++ server/src/main/java/com/example/api/service/AnimalService.java
@@ -2,17 +2,52 @@ package com.example.api.service;
 
 
 import com.example.api.entity.AnimalEntity;
+import com.example.api.entity.ClienteEntity;
+import com.example.api.entity.form.AnimalForm;
 import com.example.api.repository.AnimalRepository;
+import com.example.api.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AnimalService {
 
     @Autowired
-    private AnimalRepository repository;
+    private AnimalRepository animalRepository;
 
-    public AnimalEntity saveAnimal(AnimalEntity animal) {
-        return repository.save(animal);
+    @Autowired
+    private ClienteRepository clienteRepository;
+
+    public AnimalEntity saveAnimal(AnimalForm animal) {
+        ClienteEntity cliente = clienteRepository.burcarPorId(animal.getIdCliente());
+        if(cliente == null){
+            return null;
+        }else {
+            AnimalEntity entity = new AnimalEntity();
+            entity.setNome(animal.getNome());
+            entity.setIdCliente(cliente);
+            entity.setCor(animal.getCor());
+            entity.setDataNascimento(animal.getDataNascimento());
+            entity.setPeso(animal.getPeso());
+            entity.setTamanho(animal.getTamanho());
+            entity.setEspecie(animal.getEspecie());
+            entity.setSexo(animal.getSexo());
+            if(animal.getRaca() != null){
+                entity.setRaca(animal.getRaca());
+            }
+            return animalRepository.save(entity);
+        }
+    }
+
+    public List<AnimalEntity> buscarAnimal(Long id){
+        return animalRepository.buscarAnimais(id);
+    }
+
+    public void deleteAnimal(Long id){animalRepository.deleteById(id);}
+
+    public AnimalEntity alterarAnimal(AnimalEntity animal) {
+        return  animalRepository.save(animal);
     }
 }
--- server/src/main/java/com/example/api/service/ClienteService.java
+++ server/src/main/java/com/example/api/service/ClienteService.java
@@ -1,10 +1,12 @@
 package com.example.api.service;
 
 import com.example.api.entity.ClienteEntity;
+import com.example.api.entity.form.ClienteForm;
 import com.example.api.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 public class ClienteService {
@@ -14,15 +16,41 @@ public class ClienteService {
     private ClienteRepository repository;
 
     public ClienteEntity getClienteByEmailSenha(String email, String senha){
-        return repository.buscarPorEmailSenha(email, senha);
+        Optional<ClienteEntity> entityOptional = Optional.ofNullable(repository.buscarPorEmailSenha(email, senha));
+        return entityOptional.orElseThrow(() -> new RuntimeException("Cliente não encontrado"));
     }
 
-    @Transactional
-    public ClienteEntity saveCliente(ClienteEntity cliente){
-        return repository.save(cliente);
+
+
+    public ClienteEntity saveCliente(ClienteForm cliente){
+
+        System.out.println(cliente);
+        if(repository.buscarCpf(cliente.getCpf()) != null || repository.buscarEmail(cliente.getEmail()) != null) {
+            return null;
+        }else {
+            ClienteEntity entity = new ClienteEntity();
+            entity.setNome(cliente.getNome());
+            entity.setDataNascimento(cliente.getDataNascimento());
+            entity.setCpf(cliente.getCpf());
+            entity.setEndereco(cliente.getEndereco());
+            entity.setEmail(cliente.getEmail());
+            entity.setSenha(cliente.getSenha());
+            if(cliente.getTelefone() != null){
+                entity.setTelefone(cliente.getTelefone());
+            }
+            System.out.println("criou objeto");
+            return repository.save(entity);
+        }
     }
 
     public ClienteEntity getById(Long idCliente) {
         return repository.burcarPorId(idCliente);
     }
+
+    public ClienteEntity alteararCliente(ClienteEntity cliente){
+        return repository.save(cliente);
+    }
+
+    public void delete(Long id){repository.deleteById(id);}
+
 }
