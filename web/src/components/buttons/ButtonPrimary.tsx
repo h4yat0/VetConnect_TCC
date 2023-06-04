f9cf7ed831556b812d662b5e6c863bf4819b1592
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-06-04 20:12:01 -0300

    [UPDATE] more options for button primary

--- web/src/components/buttons/ButtonPrimary.tsx
+++ web/src/components/buttons/ButtonPrimary.tsx
@@ -1,29 +1,38 @@
+import { MouseEventHandler } from "react";
+
 interface buttonPrimaryProps {
 	text: string;
+	type?: "button" | "submit" | "reset";
+	width?: "w-full";
+	onClickFunction?: () => void;
 }
 
 export default function buttonPrimary(props: buttonPrimaryProps) {
+	const { type = "button", text, width, onClickFunction } = props;
+
+	const buttonClassName = `
+        ${width}
+        bg-vetConnectPrimaryGreen
+        text-gray-800
+        py-2 
+        px-4 
+        rounded-sm 
+        text-sm 
+        font-bold
+        cursor-pointer 
+        transition-all 
+        duration-200 
+        ease-in-out 
+        hover:bg-vetConnectSecundaryGreen 
+        hover:text-vetConnectGray
+        transform 
+        hover:scale-105 
+        focus:outline-none
+    `;
+
 	return (
-		<button
-			className='mx-8 
-			bg-vetConnectPrimaryGreen
-            text-gray-800
-            py-2 
-            px-4 
-            rounded-sm 
-            text-sm 
-            font-bold
-            cursor-pointer 
-            transition-all 
-            duration-200 
-            ease-in-out 
-            hover:bg-vetConnectSecundaryGreen 
-            hover:text-vetConnectGray
-            transform 
-            hover:scale-105 
-            focus:outline-none'
-		>
-			{props.text}
+		<button type={type} className={buttonClassName} onClick={onClickFunction}>
+			{text}
 		</button>
 	);
 }
