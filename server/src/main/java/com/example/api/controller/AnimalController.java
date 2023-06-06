8132463ce66fb9304df6566b59fc5ad897b47803
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-06-06 19:34:03 -0300

    [UPDATE] Get para animais do cliente

--- server/src/main/java/com/example/api/controller/AnimalController.java
+++ server/src/main/java/com/example/api/controller/AnimalController.java
@@ -10,7 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/animal")
 public class AnimalController {
