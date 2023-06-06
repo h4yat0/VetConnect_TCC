5e64e349f68e337b75ee4797ed1a9d32268636fe
Gabriel Arthur Tonholo de Campos <gabrielarthurtonholodecampos@gmail.com>
2023-06-06 20:55:28 -0300

    [Update] Service Cards

--- web/src/components/ServiceCard.tsx
+++ web/src/components/ServiceCard.tsx
@@ -1,24 +1,39 @@
+import { TbVaccine } from 'react-icons/tb';
+import { MdPets } from 'react-icons/md';
+import {FaShower} from 'react-icons/fa';
+import {IoCutSharp} from 'react-icons/io5';
+
 interface ServiceCardProps {
 	title: string;
 	iconName: string;
 	serviceId: number;
+	
 }
 
+function iconSelect(title:string): any{
+	if(title == "Banho") return <FaShower className="h-10 w-10"/>;
+	if(title == "Tosa") return <IoCutSharp className="h-10 w-10"/>;
+	if(title == "Castração") return <MdPets className="h-10 w-10"/>;
+	if(title == "Vacinação") return <TbVaccine className="h-10 w-10"/>;
+	return <MdPets className="h-10 w-10"/>;
+}
+	
 export default function ServiceCard(props: ServiceCardProps) {
 	const { title, iconName, serviceId } = props;
 
-	return (
 	
-		
-		<div className=" bg-gray-100 grid items-center justify-center">
-		  <div className="p-4 bg-white flex  space-x-2 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
-			<div>
-			  <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-indigo-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-				<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-			  </svg>
+
+	return (
+		<div className="w-[365px] bg-gray-100 grid items-center justify-center">
+		  <div className="w-[365px] p-4 bg-white flex  space-x-4 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
+			<div className="mt-4 ml-2">
+			
+			{iconSelect(title)} 
+			
 			</div>
-			<div>
-			  <h1 className="text-xl font-bold text-gray-700 mb-2">{title}</h1>
+			<div className="w-[365px]">
+			<div className="absolute left-1/4 w-0.5 h-3/4 bg-gray-600"></div>
+			<h1 className="mt-4 ml-2 p-2 text-center text-xl font-bold text-gray-700 mb-4">{title}</h1>
 				</div>
 		  </div>
 		</div>
