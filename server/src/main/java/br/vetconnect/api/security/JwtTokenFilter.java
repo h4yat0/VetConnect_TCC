2365f64145b64fe93bc6e4fd7df370a12567b81b
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-09-30 19:34:50 -0300

    colocando try catch no jwtfilter, para interceptar as exceções

--- server/src/main/java/br/vetconnect/api/security/JwtTokenFilter.java
+++ server/src/main/java/br/vetconnect/api/security/JwtTokenFilter.java
@@ -1,14 +1,19 @@
 package br.vetconnect.api.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import exceptions.ExceptionResponse;
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.GenericFilterBean;
 
 import java.io.IOException;
+import java.util.Date;
 
 public class JwtTokenFilter extends GenericFilterBean {
 
@@ -22,15 +27,31 @@ public class JwtTokenFilter extends GenericFilterBean {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        String token = tokenProvider.resolveToken((HttpServletRequest) servletRequest);
+        try {
+            String token = tokenProvider.resolveToken((HttpServletRequest) servletRequest);
 
-        if(token != null && tokenProvider.validateToken(token)){
-            Authentication auth = tokenProvider.getAuthentication(token);
-            if(auth != null){
-                SecurityContextHolder.getContext().setAuthentication(auth);
+            if(token != null && tokenProvider.validateToken(token)){
+                Authentication auth = tokenProvider.getAuthentication(token);
+                if(auth != null){
+                    SecurityContextHolder.getContext().setAuthentication(auth);
 
+                }
             }
+            filterChain.doFilter(servletRequest, servletResponse);
+        }catch (Exception e){
+            e.printStackTrace();
+
+            HttpServletResponse response = (HttpServletResponse) servletResponse;
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR); // Define o código de status 500
+
+
+            ExceptionResponse errorResponse = new ExceptionResponse(new Date(), HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+            String jsonResponse = new ObjectMapper().writeValueAsString(errorResponse);
+
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+            response.getWriter().write(jsonResponse);
         }
-        filterChain.doFilter(servletRequest, servletResponse);
+
     }
 }
