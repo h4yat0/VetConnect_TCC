26387fc71264694b02df062d0890462d30e23cdb
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-09-30 20:00:52 -0300

    form para as unidades

---
+++ server/src/main/java/br/vetconnect/api/form/Unidade/UnidadeFormCreate.java
@@ -0,0 +1,59 @@
+package br.vetconnect.api.form.Unidade;
+
+
+
+
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnidadeFormCreate {
+
+    @Valid
+    @NotBlank
+    private String nome;
+    @Valid
+    @NotBlank
+    private String especialidade;
+    @Valid
+    @NotBlank
+    private String horarioFuncionamento;
+    @Valid
+    @NotBlank
+    private String contato;
+    @Valid
+    @NotBlank
+    private String cep;
+
+    @Valid
+    @NotBlank
+    private String bairro;
+    @Valid
+    @NotBlank
+    private String cidade;
+    @Valid
+    @NotBlank
+    private String estado;
+    @Valid
+    @NotBlank
+    private String complemento;
+    @Valid
+    @NotBlank
+    private String rua;
+    @Valid
+    @NotBlank
+    private String cnpj;
+
+    private Integer numero;
+
+    private List<String> imagem;
+
+
+}
---
+++ server/src/main/java/br/vetconnect/api/form/Unidade/UnidadeFormReturn.java
@@ -0,0 +1,40 @@
+package br.vetconnect.api.form.Unidade;
+
+import br.vetconnect.api.entity.Agendamento.ServicoEntity;
+import br.vetconnect.api.entity.imagens.UnidadeImagensEntity;
+import br.vetconnect.api.form.Cliente.ClienteFormCreate;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UnidadeFormReturn extends RepresentationModel<UnidadeFormReturn> implements Serializable {
+
+
+    private Long id;
+    private String nome;
+    private String especialidade;
+    private String horarioFuncionamento;
+    private String contato;
+    private String cep;
+    private List<ServicoEntity> servicos;
+    private String bairro;
+    private String cidade;
+    private String estado;
+    private String complemento;
+    private Integer numero;
+    private String cnpj;
+    private String rua;
+    private List<byte[]> imagem;
+
+
+
+}
