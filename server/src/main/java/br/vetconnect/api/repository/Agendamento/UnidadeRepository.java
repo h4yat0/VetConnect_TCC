0fa0a68a66958da96bc72b49efd3e91ff50b42fd
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-09-30 19:43:27 -0300

    novos metodos no repository da unidade

--- server/src/main/java/br/vetconnect/api/repository/Agendamento/UnidadeRepository.java
+++ server/src/main/java/br/vetconnect/api/repository/Agendamento/UnidadeRepository.java
@@ -6,9 +6,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UnidadeRepository extends JpaRepository<UnidadeEntity,Long> {
 
     @Query(value = "SELECT * from unidade where id = ?1", nativeQuery = true)
     UnidadeEntity buscarUnidade(Long id);
+
+    @Query(value = "SELECT * from unidade where cnpj = ?1", nativeQuery = true)
+    UnidadeEntity buscarUnidadePorCnpj(String cnpj);
+
+    @Query(value = "select * from unidade where id in (select unidade_id from servico_unidade);", nativeQuery = true)
+    List<UnidadeEntity> buscarUnidadesComServico();
+
 }
