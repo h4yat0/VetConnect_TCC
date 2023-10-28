d599e6bc4112f9fdff11461dec287f2f06f498df
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-10-28 14:22:57 -0300

    Novo select para serviços

--- server/src/main/java/br/vetconnect/api/repository/Agendamento/ServicoRepository.java
+++ server/src/main/java/br/vetconnect/api/repository/Agendamento/ServicoRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ServicoRepository extends JpaRepository<ServicoEntity, Long> {
 
 
     @Query(value = "SELECT * from servico where id = ?1", nativeQuery = true)
     ServicoEntity buscarServico(Long id);
+
+    @Query(value = "SELECT * FROM servico where id in (select servico_id from servico_unidade)", nativeQuery = true)
+    List<ServicoEntity> buscarServicosComUnidade();
 }
