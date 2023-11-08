d7918d1b8a94c2cd6d9572fef9d978c05ccbc9b4
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-11-07 22:08:21 -0300

    commit feito na faculdade

--- server/src/main/java/br/vetconnect/api/controller/Agendamento/FilaDeEsperaController.java
+++ server/src/main/java/br/vetconnect/api/controller/Agendamento/FilaDeEsperaController.java
@@ -1,8 +1,10 @@
 package br.vetconnect.api.controller.Agendamento;
 
 
+import br.vetconnect.api.form.FilaDeEsperaFormCreate;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,4 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class FilaDeEsperaController {
 
 
+    public ResponseEntity<?> cadastraEspera(FilaDeEsperaFormCreate formCreate) {
+        return null;
+    }
 }
--- server/src/main/java/br/vetconnect/api/service/Agendamento/FilaDeEsperaService.java
+++ server/src/main/java/br/vetconnect/api/service/Agendamento/FilaDeEsperaService.java
@@ -1,6 +1,9 @@
 package br.vetconnect.api.service.Agendamento;
 
 
+import br.vetconnect.api.controller.Agendamento.FilaDeEsperaController;
+import br.vetconnect.api.controller.ClienteController;
+import br.vetconnect.api.entity.Agendamento.AgendamentoEntity;
 import br.vetconnect.api.entity.Agendamento.FilaEsperaEntity;
 import br.vetconnect.api.form.FilaDeEsperaFormCreate;
 import br.vetconnect.api.form.FilaDeEsperaFormReturn;
@@ -8,6 +11,9 @@ import br.vetconnect.api.mapper.FilaDeEsperaMapper;
 import br.vetconnect.api.repository.Agendamento.FilaDeEsperaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Service
 public class FilaDeEsperaService {
@@ -18,9 +24,15 @@ public class FilaDeEsperaService {
     @Autowired
     private FilaDeEsperaMapper mapper;
 
+    @Autowired
+    private AgendamentoService agendamentoService;
+
 
     public FilaDeEsperaFormReturn cadastraEspera(FilaDeEsperaFormCreate formCreate){
-        FilaEsperaEntity entity = mapper.formCreateParaEntity(formCreate);
-        return null;
+        AgendamentoEntity agendamentoEntity = agendamentoService.buscarPorId(formCreate.getIdAgendamento());
+        FilaEsperaEntity entity = mapper.formCreateParaEntity(formCreate, agendamentoEntity);
+        FilaDeEsperaFormReturn formReturn = mapper.entityParaFormReturn(repository.save(entity));
+        formReturn.add(linkTo(methodOn(FilaDeEsperaController.class).cadastraEspera(formCreate)).withSelfRel());
+        return formReturn;
     }
 }
