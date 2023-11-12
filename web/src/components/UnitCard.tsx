7c45e0761c8a592f04de69ec2c8ce34047124f3e
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-11 22:45:34 -0300

    better routing on unit cards

--- web/src/components/UnitCard.tsx
+++ web/src/components/UnitCard.tsx
@@ -1,21 +1,22 @@
+import { Link, useNavigate } from "react-router-dom";
+
 interface UnitCardProps {
-	title: string;
-	imgPath: string;
-	clinicId: number;
+  title: string;
+  image: string;
+  unitId: number;
 }
 export default function UnitCard(props: UnitCardProps) {
-	const { title, imgPath, clinicId } = props;
+	const navigate = useNavigate()
+	const { title, image, unitId } = props;
 
 	return (
-		<div className='font-inter'>
-			<a href={`unidades/${clinicId}`}>
-				<img
-					src={imgPath}
-					alt=''
-					className='h-36 w-[365px] shadow-md rounded-lg object-cover'
-				/>
-				<h3 className='pt-1 font-semibold text-lg'> {title}</h3>
-			</a>
-		</div>
-	);
+      <Link to={`/unidades/${unitId}`}>
+        <img
+          src={`data:image/webp;base64,${image}`}
+          alt=""
+          className="h-36 w-[365px] shadow-md rounded-lg object-cover"
+        />
+        <h3 className="pt-1 font-semibold text-lg">{title}</h3>
+      </Link>
+  );
 }
