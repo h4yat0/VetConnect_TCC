ce1647382abed8a6c8c9177999f9ca9f2842ab1e
Gabriel Arthur Tonholo de Campos <gabrielarthurtonholodecampos@gmail.com>
2023-11-12 00:14:02 -0300

    [UPDATE] Alert Confirm

--- web/src/components/AlertConfirm.tsx
+++ web/src/components/AlertConfirm.tsx
@@ -6,7 +6,6 @@ interface AlertModalProps {
   type: "ConfirmacaoEditar" | "ConfirmacaoSalvar" | "ConfirmacaoDeletar";
   isOpen: boolean;
   message: string;
-
   setIsOpen: (isOpen: boolean) => void;
 }
 
@@ -70,7 +69,14 @@ export default function AlertConfirm({
                     className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                     onClick={() => setIsOpen(!isOpen)}
                   >
-                    Fechar
+                    Sim
+                  </button>
+                  <button
+                    type="button"
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                    onClick={() => setIsOpen(!isOpen)}
+                  >
+                    Não
                   </button>
                 </div>
               </Dialog.Panel>
