da9cf401de41c9fedbf04d06443042d5172c1fce
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-12 16:04:59 -0300

    new buttons on modals

--- web/src/components/AlertModal.tsx
+++ web/src/components/AlertModal.tsx
@@ -1,6 +1,7 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import ButtonSecundary from "./buttons/ButtonSecundary";
 
 interface AlertModalProps {
   type: "success" | "caution" | "danger";
@@ -42,7 +43,7 @@ export default function AlertModal({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
@@ -50,7 +51,7 @@ export default function AlertModal({
                   {() => {
                     switch (type) {
                       case "success":
-                        return <span>Sucesso</span>;
+                        return <span>Operação realizada com sucesso</span>;
                       case "caution":
                         return <span>Cuidado</span>;
                       case "danger":
@@ -65,13 +66,10 @@ export default function AlertModal({
                 </div>
 
                 <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setIsOpen(!isOpen)}
-                  >
-                    Fechar
-                  </button>
+                  <ButtonSecundary
+                    text="Fechar"
+                    onClickFunction={() => setIsOpen(!isOpen)}
+                  />
                 </div>
               </Dialog.Panel>
             </Transition.Child>
