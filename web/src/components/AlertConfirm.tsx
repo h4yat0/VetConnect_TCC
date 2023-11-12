0410d4103974421ba2b05fc11363184390e8ef87
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-12 16:04:28 -0300

    new buttons and functions

--- web/src/components/AlertConfirm.tsx
+++ web/src/components/AlertConfirm.tsx
@@ -1,9 +1,11 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import ButtonPrimary from "./buttons/ButtonPrimary";
+import ButtonSecundary from "./buttons/ButtonSecundary";
 
 interface AlertModalProps {
-  type: "ConfirmacaoEditar" | "ConfirmacaoSalvar" | "ConfirmacaoDeletar";
+  type: "insert" | "edit" | "delete";
   isOpen: boolean;
   message: string;
   setIsOpen: (isOpen: boolean) => void;
@@ -17,7 +19,7 @@ export default function AlertConfirm({
 }: AlertModalProps) {
   return (
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={() => setIsOpen(!isOpen)}>
+      <Dialog as="div" className="absolute z-[11]" onClose={() => setIsOpen(!isOpen)}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -41,43 +43,23 @@ export default function AlertConfirm({
               leaveFrom="opacity-100 scale-100"
               leaveTo="opacity-0 scale-95"
             >
-              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
+              <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded bg-white p-6 text-left align-middle shadow-xl transition-all">
                 <Dialog.Title
                   as="h3"
                   className="text-lg font-medium leading-6 text-gray-900"
                 >
-                  {() => {
-                    switch (type) {
-                      case "ConfirmacaoDeletar":
-                        return <span>Confirme a deleção</span>;
-                      case "ConfirmacaoSalvar":
-                        return <span>Confirme a inserção</span>;
-                      case "ConfirmacaoEditar":
-                        return <span>Confirme a Edição</span>;
-                      default:
-                        return <span></span>;
-                    }
-                  }}
+                  <span>Confirmação</span>
                 </Dialog.Title>
                 <div className="mt-2">
-                  <p className="text-sm text-gray-500">{message}</p>
+                  <p className="text-md text-gray-800">{message}</p>
                 </div>
 
-                <div className="mt-4">
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setIsOpen(!isOpen)}
-                  >
-                    Sim
-                  </button>
-                  <button
-                    type="button"
-                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                    onClick={() => setIsOpen(!isOpen)}
-                  >
-                    Não
-                  </button>
+                <div className="mt-4 flex gap-2">
+                  <ButtonPrimary text="Sim" />
+                  <ButtonSecundary
+                    text="Não"
+                    onClickFunction={() => setIsOpen(!isOpen)}
+                  />
                 </div>
               </Dialog.Panel>
             </Transition.Child>
