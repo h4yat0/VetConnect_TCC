b7155a6d941060e9b480f3b91a25b6fa3aa68715
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-11-22 21:47:56 -0300

    Criação de uma nova role e um usuario com essa role

--- server/src/main/java/br/vetconnect/api/configs/teste.java
+++ server/src/main/java/br/vetconnect/api/configs/teste.java
@@ -2,13 +2,19 @@ package br.vetconnect.api.configs;
 
 import br.vetconnect.api.entity.ClienteEntity;
 import br.vetconnect.api.entity.security.PermissionEntity;
+import br.vetconnect.api.entity.security.UsersEntity;
 import br.vetconnect.api.repository.ClienteRepository;
 import br.vetconnect.api.repository.security.PermissionRepository;
+import br.vetconnect.api.repository.security.UsersRepository;
+import br.vetconnect.api.service.security.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 @Component
 public class teste implements CommandLineRunner {
@@ -16,12 +22,24 @@ public class teste implements CommandLineRunner {
     @Autowired
     private PermissionRepository repository;
 
+    @Autowired
+    private UsersRepository usersRepository;
+
+    @Autowired
+    private AuthService service;
+
 
     @Override
     public void run(String... args) throws Exception {
         if(repository.count() ==0){
             repository.save(new PermissionEntity(1L, "Cliente"));
             repository.save(new PermissionEntity(2L, "Funcionario"));
+            repository.save(new PermissionEntity(3L, "Gerente"));
+        }
+
+        if(usersRepository.count() == 0){
+            usersRepository.save(new UsersEntity(1L, "admin", 3, 1L, service.createPassword("senha123"),
+                    true, true, true, true, new ArrayList<>(Arrays.asList(new PermissionEntity(3L, "Gerente")))));
         }
     }
 }
