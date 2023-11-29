e35ac3f356462a18f398b19f4fa4c137e39ff546
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-29 19:49:28 -0300

    Update ServiceCard.tsx

--- web/src/components/ServiceCard.tsx
+++ web/src/components/ServiceCard.tsx
@@ -5,7 +5,6 @@ import { IoCutSharp } from "react-icons/io5";
 
 interface ServiceCardProps {
   title: string;
-  iconName?: string;
   serviceId?: number | null;
   handleSchedulingOpen: () => void;
 }
@@ -30,16 +29,14 @@ export default function ServiceCard(props: ServiceCardProps) {
 
   return (
     <div
-      className="w-full grid items-center justify-center"
+      className="w-full min-w-100 p-4 bg-white flex space-x-4 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer"
       onClick={() => handleSchedulingOpen()}
     >
-      <div className="w-full min-w-100 p-4 bg-white flex space-x-4 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
-        <div className="mt-4 ml-2">{iconSelect(title)}</div>
-        <div className="w-full">
-          <h1 className="mt-4 ml-2 p-2 text-center text-xl font-bold text-gray-700 mb-4">
-            {title}
-          </h1>
-        </div>
+      <div className="mt-4 ml-2">{iconSelect(title)}</div>
+      <div className="w-full">
+        <h1 className="mt-4 ml-2 p-2 text-center text-xl font-bold text-gray-700 mb-4">
+          {title}
+        </h1>
       </div>
     </div>
   );
