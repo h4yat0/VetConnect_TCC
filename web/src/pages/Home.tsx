944a5832ad4606b3c10d4f1e9ce30c7959848526
João Victor Hayato Tsuchiya de Oliveira <63439982+h4yat0@users.noreply.github.com>
2023-11-29 19:49:16 -0300

    Update Home.tsx

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -4,119 +4,100 @@ import PromoCard from "../components/PromoCard";
 import { useEffect, useState } from "react";
 import HistoryCard from "../components/HistoryCard";
 import useSimpleAuth from "../hooks/useSimpleAuth";
-import SchedulingModal from "../components/SchedulingModal";
-import { useSelector } from "react-redux";
+import ScheduleModal from "../components/SchedulingModal";
+import { useDispatch, useSelector } from "react-redux";
 import { getUnits } from "../redux/unit";
 import unitsAndServices from "../hooks/useStoreRestock";
 import useRestockUnitsAndServices from "../hooks/useStoreRestock";
 import AlertConfirm from "../components/AlertConfirm";
 import api from "../api/axios";
 import { PlusIcon } from "@heroicons/react/24/outline";
+import {
+  getAccessToken,
+  getId,
+  getSchedules,
+  updateSchedules,
+} from "../redux/client";
+import ScheduleCard from "../components/ScheduleCard";
+import ButtonSecundary from "../components/buttons/ButtonSecundary";
+import HistoricModal from "../components/HistoricModal";
 
-// const SERVICE_URL = "/api/servico/v1/somente-servicos";
-
-// interface ApiService {
-//   id: number;
-//   nome: string;
-//   preco: number;
-// }
-
-// interface Service {
-//   id: number;
-//   name: string;
-//   price: number;
-// }
-
-// function mapApiServicesToServices(apiServices: ApiService[]): Service[] {
-//   return apiServices.map((apiService) => ({
-//     id: apiService.id,
-//     name: apiService.nome,
-//     price: apiService.preco,
-//   }));
-// }
-
-const HistoryServices = {
-  history: [
-    {
-      id: 1,
-      clinicId: 1,
-      nome: "Banho",
-      dateService: "24/02/2023",
-    },
-    {
-      id: 2,
-      clinicId: 2,
-      nome: "Tosa",
-      dateService: "24/01/2023",
-    },
-    {
-      id: 3,
-      clinicId: 3,
-      nome: "Castração",
-      dateService: "24/01/2023",
-    },
-    {
-      id: 4,
-      clinicId: 4,
-      nome: "Vacinação",
-      dateService: "24/01/2022",
-    },
-  ],
-};
+const SCHEDULING_URL = "/api/agendamento/v1/buscarAgendamentos/";
 
 export default function Home() {
+  const dispatch = useDispatch();
   const loggedIn = useSimpleAuth();
-
+  const clientId = useSelector(getId);
+  const accessToken = useSelector(getAccessToken);
+  const schedules = [...useSelector(getSchedules)].filter((schedule) => schedule.status == 1);
   const units = [...useSelector(getUnits)];
 
   const { getUnitsAndServices } = useRestockUnitsAndServices();
 
-  // const [services, setServices] = useState<Service[]>([]);
+  const [scheduleId, setScheduleId] = useState(0);
   const [schedulingIsOpen, setSchedulingIsOpen] = useState(false);
+  const [historicIsOpen, setHistoricIsOpen] = useState(false);
   const [schedulingType, setSchedulingType] = useState<
     "new" | "inProgress" | "finished"
   >("new");
 
-  const [serviceId, setServiceId] = useState<number>(-1)
+  const [serviceId, setServiceId] = useState<number>(-1);
 
-  const handleSchedulingOpen = (
-    schedulingType: "new" | "inProgress" | "finished",
-    serviceId: number,
+  const handleNewSchedule = (
+    serviceId: number
   ) => {
     return () => {
-      setSchedulingType(schedulingType);
+      setSchedulingType('new');
       setServiceId(serviceId);
+      setScheduleId(-1)
       setSchedulingIsOpen(true);
     };
   };
 
+  const handleSchedule=(schedulingType : 'inProgress' | 'finished' ,scheduleId : number)=>{
+    return () => {
+      setSchedulingType(schedulingType);
+      setScheduleId(scheduleId);
+      setSchedulingIsOpen(true);
+    }
+  }
+
+  const getSchedulesApi = async () => {
+    let response = await api
+      .get(SCHEDULING_URL + clientId, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      })
+      .then(function (response) {
+        let data = response.data;
+        dispatch(updateSchedules(data));
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     if (units.length == 0) getUnitsAndServices();
-    // if (services.length == 0) getServices();
+    if (loggedIn) getSchedulesApi();
     setSchedulingIsOpen(false);
   }, []);
 
-  // const getServices = async () => {
-  //   let response = await api
-  //     .get(SERVICE_URL)
-  //     .then(function (response) {
-  //       let data = response.data;
-  //       setServices(mapApiServicesToServices(data).slice(0,5));
-  //     })
-  //     .catch(function (error) {
-  //       console.log(error);
-  //     });
-  // };
-
   return (
     <div className="w-full">
-      {units.length > 0 ? (
-        <SchedulingModal
-          type={schedulingType}
-          isOpen={schedulingIsOpen}
-          serviceId={serviceId}
-          setIsOpen={setSchedulingIsOpen}
-        />
+      {units.length > 0 && loggedIn ? (
+        <>
+          <ScheduleModal
+            type={schedulingType}
+            isOpen={schedulingIsOpen}
+            serviceId={serviceId}
+            scheduleId={scheduleId}
+            setIsOpen={setSchedulingIsOpen}
+          />
+
+          <HistoricModal isOpen={historicIsOpen} setIsOpen={setHistoricIsOpen} />
+        </>
       ) : null}
       <div className="p-0 font-inter ">
         <PromoCard />
@@ -124,11 +105,18 @@ export default function Home() {
         <div className="py-8">
           {loggedIn ? (
             <>
-              <h1 className="text-2xl font-black ">Agendamentos</h1>
-              <div className="flex flex-row gap-10 pt-2 pb-6">
+              <div className="flex justify-between">
+                <h1 className="text-2xl font-black ">Agendamentos</h1>
+                <ButtonSecundary
+                  text="Histórico"
+                  onClickFunction={() => setHistoricIsOpen(true)}
+                />
+              </div>
+
+              <div className="flex gap-8 pt-2 pb-6 px-2 overflow-x-auto">
                 <button
-                  className="min-w-50 p-4 bg-white flex space-x-4 rounded-lg border-4 border-dashed hover:scale-105 hover:border-vetConnectPrimaryGreen hover:text-vetConnectPrimaryGreen transition transform duration-500 cursor-pointer"
-                  onClick={handleSchedulingOpen("new", -1)}
+                  className="min-w-50 p-4 bg-white flex  items-center space-x-4 rounded-lg border-4 border-dashed hover:scale-105 hover:border-vetConnectPrimaryGreen hover:text-vetConnectPrimaryGreen transition transform duration-500 cursor-pointer"
+                  onClick={handleNewSchedule(-1)}
                 >
                   <div className="mt-4 ml-2">
                     <PlusIcon className="h-10 w-10" />
@@ -139,26 +127,24 @@ export default function Home() {
                     </h1>
                   </div>
                 </button>
-                {/* {services.map((service) => (
-                  <ServiceCard
-                    key={service.id}
-                    title={service.name}
-                    serviceId={service.id}
-                    handleSchedulingOpen={handleSchedulingOpen(service.id)}
-                  />
-                ))} */}
-              </div>
-
-              <h1 className="text-2xl font-black ">Últimos serviços utilizados</h1>
-              <div className="flex flex-row gap-20 pt-2 pb-6">
-                {HistoryServices.history.map((history) => (
-                  <HistoryCard
-                    nome={history.nome}
-                    clinicId={history.clinicId}
-                    dateService={history.dateService}
-                    key={history.id}
-                  />
-                ))}
+                {schedules.length > 0
+                  ? schedules.map((schedule) => (
+                      <ScheduleCard
+                        key={schedule.id}
+                        id={schedule.id}
+                        status={schedule.status}
+                        animal={schedule.animal}
+                        unit={schedule.unit}
+                        service={schedule.service}
+                        scheduleDate={schedule.scheduledDate}
+                        scheduleTime={schedule.scheduledTime}
+                        handleSchedulingOpen={handleSchedule(
+                          "inProgress",
+                          schedule.id
+                        )}
+                      />
+                    ))
+                  : null}
               </div>
             </>
           ) : null}
