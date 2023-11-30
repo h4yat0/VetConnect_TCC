051d886c2b1bc8627ee0c1cd3332aa00b6155c38
Gabriel Arthur Tonholo de Campos <gabrielarthurtonholodecampos@gmail.com>
2023-11-29 21:34:15 -0300

    [Update]

--- web/src/components/FuncionarioCard.tsx
+++ web/src/components/FuncionarioCard.tsx
@@ -6,7 +6,7 @@ import { IoCutSharp } from "react-icons/io5";
 interface AgendamentosFuncionarios {
   nomeDono: string;
   nomeAnimal: string;
-  servico:string;
+  servico: string;
   porte: string;
   preco: number;
   data: string;
@@ -17,17 +17,25 @@ export default function FuncionarioCard(props: AgendamentosFuncionarios) {
   const { nomeDono, nomeAnimal, servico, porte, preco, data } = props;
 
   return (
-    <div className="w-full min-w-100 p-4 bg-white flex space-x-4 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
-      <div className="mt-4 ml-2">Nome do dono:{nomeDono}</div>
-      <div className="mt-4 ml-2">Animal:{nomeAnimal}</div>
-      <div className="mt-4 ml-2">Tamanho:{porte}</div>
-      <div className="mt-4 ml-2">Serviço:{servico}</div>
-      <div className="mt-4 ml-2">Preço:{preco}</div>
-      <div className="w-full">{data}
-        <h1 className="mt-4 ml-2 p-2 text-center text-xl font-bold text-gray-700 mb-4">
-        x
-        </h1>
+    <div className="mr-2 grid grid-cols-3 gap-4 border border-gray-500 mb-4 mt-4 w-full min-w-100 p-4 bg-white space-x-2 rounded-lg shadow-md hover:scale-105 transition transform duration-500 cursor-pointer">
+      <div className="ml-2 col-span-2">
+        <div>Nome do dono: {nomeDono}</div>
+        <div>Animal: {nomeAnimal}</div>
+        <div>Tamanho: {porte}</div>
+        <div>Serviço: {servico}</div>
       </div>
+      <div className="mr-4 text-end  pl-11 grid grid-cols-2 gap-2 justify-center  ">
+        <div className="..."></div>
+          <div className="... mr-2">{data}</div>
+          <div className="col-span-2 ..."> </div>
+          <div className="..."></div>
+          <div className="..."></div>
+          <div className="col-span-2 ..."></div>
+          <div className="..."></div>
+          <div className="..."></div>
+          <div className="col-span-2  mr-2  ...">Preço: {preco}</div>
+        </div>
+
     </div>
   );
 }
