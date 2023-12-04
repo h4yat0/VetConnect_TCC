e552762d49c6c804f5f1e6fc856d435c376d7596
LFMO1 <90411324+LFMO1@users.noreply.github.com>
2023-12-04 20:31:50 -0300

    arrumando texto de email e adicionando nova dependencia

--- server/src/main/java/br/vetconnect/api/service/Email/EmailFilaDeEsperaService.java
+++ server/src/main/java/br/vetconnect/api/service/Email/EmailFilaDeEsperaService.java
@@ -4,6 +4,8 @@ package br.vetconnect.api.service.Email;
 import br.vetconnect.api.form.EmailFilaDeEspera;
 import br.vetconnect.api.repository.Agendamento.FiilaEspera.FilaDeEsperaRepository;
 import jakarta.mail.MessagingException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -32,11 +34,11 @@ public class EmailFilaDeEsperaService {
                 var mensagem = emailSender.createMimeMessage();
                 var helper = new MimeMessageHelper(mensagem, true);
                 helper.setTo(email.getEmailCliente());
-                helper.setSubject("Desistencia");
-                helper.setText("Olá, "+email.getNomeCliente() +" Estamos entrando em contato pois houve uma desistencia no agendamento das "+ email.getHoraAgendada() +
+                helper.setSubject("Fila de Espera");
+                helper.setText("Olá, "+email.getNomeCliente() +" estamos entrando em contato, pois houve uma desistencia no agendamento das "+ email.getHoraAgendada() +
                         " no dia " + formataData(email.getDataAgendada()) +"\n"+
-                        "Gostaria de realizar o agendamento para o serviço " + email.getNomeServico() + " na unidade "+email.getNomeUnidade() +"?"+
-                        "Entrar em contato: " + email.getContatoUnidade());
+                        "Gostaria de realizar o agendamento para o serviço " + email.getNomeServico() + " na unidade "+email.getNomeUnidade() +"? \n"+
+                        "Entrar em contato: " + formataNumeroTelefone(email.getContatoUnidade()));
 
                 helper.setTo(email.getEmailCliente());
                 emailSender.send(mensagem);
@@ -56,6 +58,23 @@ public class EmailFilaDeEsperaService {
         }catch (Exception e){
             e.printStackTrace();
             return "";
+        }
+
+    }
+
+    private String formataNumeroTelefone(String telefone){
+        String numericPhoneNumber = StringUtils.getDigits(telefone);
+
+        // Verificar se o número é válido
+        if (NumberUtils.isParsable(numericPhoneNumber) && numericPhoneNumber.length() == 10) {
+            // Formatar como (XXX) XXX-XXXX
+            return String.format("(%s) %s-%s",
+                    numericPhoneNumber.substring(0, 2),
+                    numericPhoneNumber.substring(2, 6),
+                    numericPhoneNumber.substring(6));
+        } else {
+            // Se o número não for válido, retornar a string original
+            return telefone;
         }
 
     }
--- server/src/main/java/br/vetconnect/api/service/Email/NotificacaoService.java
+++ server/src/main/java/br/vetconnect/api/service/Email/NotificacaoService.java
@@ -8,6 +8,8 @@ import jakarta.mail.MessagingException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.persistence.Query;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
@@ -15,7 +17,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -33,7 +37,7 @@ public class NotificacaoService {
     @Autowired
     private AgendamentoService service;
 
-    @Scheduled(cron = "0 00 05 * * ?", zone = "")
+    @Scheduled(cron = "0 50 19 * * ?", zone = "")
     public void enviarEmail() throws MessagingException {
         try{
             LocalDate dataAtual = LocalDate.now();
@@ -50,17 +54,17 @@ public class NotificacaoService {
                 helper.setTo(entity.getEmailCliente());
                 helper.setSubject("Consulta");
                 if(entity.getDataAgendamento().equals(dataAtual.toString())){
-                    helper.setText("Olá " +entity.getNomeCliente() + "Espero que esteja tudo bem. Gostaríamos de lembrá-lo de que você tem um " +
-                            "agendamento, "+entity.getNomeServico()+" para o seu adorável animalzinho"+ entity.getNomeAnimal()+ "hoje, " + dataAtual +" " + entity.getHoraAgendamento() +
-                            "Na unidade " + entity.getNomeUnidade() +
-                            "\n localizada em:  " + entity.getRuaUnidade() + ", " + entity.getBairroUnidade() + ", " + entity.getCidadeUnidade() +". \n"+
-                            "Para qualquer duvida entrar em contato no numero: " +entity.getContatoUnidade()
+                    helper.setText("Olá " +entity.getNomeCliente() + ". Espero que esteja tudo bem. Gostaríamos de lembrá-lo de que você tem um " +
+                            "agendamento, "+entity.getNomeServico()+" para o seu adorável animalzinho "+ entity.getNomeAnimal()+ " hoje, " + formataData(dataAtual.toString()) +" as " + entity.getHoraAgendamento() +
+                            " Na unidade " + entity.getNomeUnidade() +
+                            "\nlocalizada em:  " + entity.getRuaUnidade() + ", " + entity.getBairroUnidade() + ", " + entity.getCidadeUnidade() +". \n"+
+                            "Para qualquer duvida entrar em contato no numero: " +formataNumeroTelefone(entity.getContatoUnidade())
                     );
                 }else{
                     helper.setText("Olá " +entity.getNomeCliente() + "Espero que esta mensagem o encontre bem. Gostaríamos de lembrá-lo de que você tem um " +
                             "agendamento, "+entity.getNomeServico()+" para o seu adorável animalzinho"+ entity.getNomeAnimal()+ "daqui dois dias, nas horas " + entity.getHoraAgendamento() +
-                            "Na unidade " + entity.getNomeUnidade() +
-                            "\n localizada em:  " + entity.getRuaUnidade() + ", " + entity.getBairroUnidade() + ", " + entity.getCidadeUnidade()
+                            " Na unidade " + entity.getNomeUnidade() +
+                            "\nlocalizada em:  " + entity.getRuaUnidade() + ", " + entity.getBairroUnidade() + ", " + entity.getCidadeUnidade()
                     );
                 }
 
@@ -71,6 +75,37 @@ public class NotificacaoService {
             }
         }catch (Exception e){
             e.printStackTrace();
+        }
+
+    }
+
+    private String formataNumeroTelefone(String telefone){
+        String numericPhoneNumber = StringUtils.getDigits(telefone);
+
+        // Verificar se o número é válido
+        if (NumberUtils.isParsable(numericPhoneNumber) && numericPhoneNumber.length() == 10) {
+            // Formatar como (XXX) XXX-XXXX
+            return String.format("(%s) %s-%s",
+                    numericPhoneNumber.substring(0, 2),
+                    numericPhoneNumber.substring(2, 6),
+                    numericPhoneNumber.substring(6));
+        } else {
+            // Se o número não for válido, retornar a string original
+            return telefone;
+        }
+
+    }
+
+    public String formataData(String data){
+        try {
+            SimpleDateFormat formatoEntrada = new SimpleDateFormat("yyyy-MM-dd");
+            Date data1 = formatoEntrada.parse(data);
+            SimpleDateFormat formatoSaida = new SimpleDateFormat("dd/MM/yyyy");
+            String dataFormatada = formatoSaida.format(data1);
+            return dataFormatada;
+        }catch (Exception e){
+            e.printStackTrace();
+            return "";
         }
 
     }
